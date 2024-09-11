3ea48c10f5c9762b6663274ceeb8edf026184f2b
DAVID LIN <908060260@QQ.COM>
2024-09-11 17:47:36 +0800

    e

--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,8 @@
 using DVLib.LabDataHelper;
 using DVOSLib;
+using MathBase;
 using System.Diagnostics;
+using System.Diagnostics.Metrics;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -11,7 +13,11 @@ namespace LabDataHelper
 		DataManager manager = new DataManager("数据");
 		MathObjectManager managerM = new MathObjectManager();
 		DataConverter converter;
+		DataConverter refConverter;
 		Helper helper;
+		LevelGetter getter = new LevelGetter();
+		DoubleStack<string> codeRecord = new();
+		double maxRef = double.PositiveInfinity;
 
 		DataSet baseSet
 		{
@@ -34,6 +40,12 @@ namespace LabDataHelper
 		public Form1()
 		{
 			InitializeComponent();
+			getter.register('(', 1);
+			getter.register(')', -1);
+			getter.register('{', 1);
+			getter.register('}', -1);
+			getter.register('[', 1);
+			getter.register(']', -1);
 			comboBox1.KeyDown += (o, e) =>
 			{
 
@@ -45,7 +57,18 @@ namespace LabDataHelper
 				}
 			};
 
-		
+			textBox2.MouseWheel += (o, e) =>
+			{
+				if (e.Delta > 0)
+				{
+					lastCode();
+				}
+				else
+				{
+					nextCode();
+				}
+			};
+
 			helper = new Helper(manager);
 			saveFileDialog1.FileOk += (o, e) =>
 			{
@@ -84,15 +107,34 @@ namespace LabDataHelper
 			)
 		{
 
-			managerM.regiseterMethod("setText", (a, b) => {
+			managerM.regiseterMethod("setText", (a, b) =>
+			{
 				if (b != null && b.Length > 0)
 				{
-					richTextBox4.Text = b[0].Item1;
-					return (null, (data) => 1);
+					string s = b[0].Item1;
+					var v = managerM.Run(b[0].Item1);
+					double d =v .getValue(2);
+					if(!double.IsNaN(d))
+					{
+						s = d.ToString();
+					}
+					if(v is RuntimeMathObject)
+					{
+					 var ss=v.AsRuntime().valueOut;
+						if(ss!=null)
+						{
+							s= ss.ToString();
+						}
+					}
+					richTextBox4.Text +=s+"\n";
+					return (s, (data) => 1);
 				}
 				return (null, (data) => 0);
+			});
+			managerM.regiseterMethod("getName", (a, b) =>
+			{
 
-
+				return (this.Text, d => 0);
 			});
 		}
 		void addVisualFx()
@@ -280,7 +322,8 @@ namespace LabDataHelper
 		void readDescribe(string s)
 		{
 
-			s = s.Trim();
+			s = s.Replace(" ","").Replace("\n", "");
+
 			var p = s.findString("unit=");
 			if (p.Count > 0)
 			{
@@ -294,10 +337,11 @@ namespace LabDataHelper
 			if (p.Count > 0)
 			{
 				int pos = p.First();
-				var v = s.AsSpan(pos + 4);
-				int end = v.findFirstChar('}');
-				string runs = v.Slice(0, end).ToString();
-				string[] strings = runs.Split(';', StringSplitOptions.RemoveEmptyEntries);
+				var v = s.AsSpan(pos + 3);
+				int end = v.findEnd('{','}');
+				string runs = v.Slice(1, end).ToString();
+				
+				string[] strings = runs.cutZeroLevel(';',getter);
 				foreach (var vvv in strings)
 				{
 					managerM.Run(vvv).getValue();
@@ -311,10 +355,28 @@ namespace LabDataHelper
 				int end = v.findFirstChar(';');
 				string ss = v.Slice(0, end).ToString();
 				var cv = managerM.Run(ss);
-
-
-
 				converter = d => cv.getValue(d);
+			}
+			p = s.findString("dataRef=");
+			if (p.Count > 0)
+			{
+				int pos = p.First();
+				var v = s.AsSpan(pos + 8);
+				int end = v.findFirstChar(';');
+				string ss = v.Slice(0, end).ToString();
+				var cv = managerM.Run(ss);
+				refConverter = d => cv.getValue(d);
+
+			}
+			p = s.findString("maxRef=");
+			if (p.Count > 0)
+			{
+				int pos = p.First();
+				var v = s.AsSpan(pos + 7);
+				int end = v.findFirstChar(';');
+				string ss = v.Slice(0, end).ToString();
+				var cv = managerM.Run(ss);
+				maxRef = cv.getValue(d);
 
 			}
 
@@ -342,11 +404,14 @@ namespace LabDataHelper
 			if (comboBox1.SelectedItem is DataSet)
 			{
 				DataSet set = (DataSet)comboBox1.SelectedItem;
+				int index = comboBox1.SelectedIndex;
 				StringBuilder sb = new StringBuilder();
 				List<int> greenPos = new List<int>();
 				List<int> greenLength = new List<int>();
 				List<int> bluePos = new List<int>();
 				List<int> blueLength = new List<int>();
+				List<int> redPos = new List<int>();
+				List<int> redLength = new List<int>();
 				int line = 0;
 				for (int i = 0; i < set.Count; i++)
 				{
@@ -356,12 +421,58 @@ namespace LabDataHelper
 				}
 				sb.AppendLine("[" + set.name + "]" + " [数据长度:(" + set.Count + ")] [平均值:(" + set.getMean(converter) + unit + ")] [极限偏差:(" + (converter(set.Max - set.Min)) + unit + ")]");
 				line++;
+				if(index>0)
+				{
+					double dy = converter(set.Mean - manager[index - 1].Mean);
+					sb.AppendLine("[变化:(" + dy+ unit + ")]");
+					line++;
+
+				}
 				if (baseSet != null)
 				{
+
 					var s = baseSet;
 					sb.AppendLine("[" + s.name + "]" + " [数据长度:(" + s.Count + ")] [平均值:(" + s.getMean(converter) + unit + ")] [极限偏差:(" + (converter(s.Max - s.Min)) + unit + ")]");
 					line++;
 					sb.AppendLine("[比较]" + " [平均值变化:(" + converter(set.Mean - s.Mean) + unit + ")] [极限偏差变化:(" + (converter(set.Max - set.Min - s.Max + s.Min)) + unit + ")]");
+					line++;
+
+				}
+
+				if (checkBox2.Checked && manager.Count > 0)
+				{
+					DataConverter rc = d => d;
+					if (refConverter != null)
+					{
+						rc = refConverter;
+					}
+					double[] rdata = manager.getDataFromMean(index + 1, converter);
+					double[] refdata = manager.getRefData(manager.getDataFromDescribe(index + 1, refConverter), rdata[0]);
+					//r2
+					double refd = refdata[index].keep(2);
+					double readd = rdata[index].keep(2);
+					double r2 = DataManager.CalculateRSquared(refdata, rdata);
+					sb.AppendLine("[参考]" + "[参考值为: " + refd + unit + "] [测量值为:" + readd + unit + "] ");
+					line++;
+					if (Math.Abs(refd - readd) <maxRef)
+					{
+						addWithColor("[相差:" + (refd - readd).keep(2) + unit + "] ", sb, greenPos, greenLength, line);
+					}
+					else
+					{
+
+						addWithColor("[相差:" + (refd - readd).keep(2) + unit + "] ", sb, redPos, redLength, line);
+					}
+					if (r2 >= (double)numericUpDown4.Value)
+					{
+						addWithColor("[R2:" + r2 + "]", sb, greenPos, greenLength, line);
+					}
+					else
+					{
+
+						addWithColor("[R2:" + r2 + "]", sb, redPos, redLength, line);
+					}
+
 					line++;
 				}
 
@@ -378,6 +489,12 @@ namespace LabDataHelper
 					richTextBox3.SelectionStart = greenPos[i];
 					richTextBox3.SelectionLength = greenLength[i];
 					richTextBox3.SelectionColor = Color.Green;
+				}
+				for (int i = 0; i < redLength.Count; i++)
+				{
+					richTextBox3.SelectionStart = redPos[i];
+					richTextBox3.SelectionLength = redLength[i];
+					richTextBox3.SelectionColor = Color.Red;
 				}
 				richTextBox3.Select(richTextBox3.Text.Length - 1, 0);
 				richTextBox3.ScrollToCaret();
@@ -404,8 +521,8 @@ namespace LabDataHelper
 			if (comboBox1.SelectedItem is DataSet)
 			{
 
-				manager.addValue(comboBox1.SelectedIndex, managerM.Run(textBox2.Text).getValue(1));
-				textBox2.Text = "";
+				manager.addValue(comboBox1.SelectedIndex, managerM.Run(textBox2.Text).getValue(1,2,3));
+				codeInput();
 				textBox2.Focus();
 
 			}
@@ -460,7 +577,7 @@ namespace LabDataHelper
 			converter = null;
 			unit = null;
 			managerM.clear();
-			
+
 			if (!Directory.Exists("data"))
 			{
 				Directory.CreateDirectory("data");
@@ -475,7 +592,7 @@ namespace LabDataHelper
 		private void button6_Click(object sender, EventArgs e)
 		{
 			lastSelectDataset = -1;
-		
+
 			if (File.Exists("data\\" + manager.name + ".data"))
 			{
 				manager.load("data\\" + manager.name + ".data");
@@ -483,7 +600,7 @@ namespace LabDataHelper
 				richTextBox1.Text = manager.describe;
 				converter = null;
 				unit = null;
-				managerM.clear();	
+				managerM.clear();
 				registerFunc();
 				readDescribe(manager.describe);
 			}
@@ -510,12 +627,30 @@ namespace LabDataHelper
 			comboBox1.SelectedItem = null;
 			lastSelectDataset = -1;
 		}
+		
+		void codeInput()
+		{
+			if(textBox2.Text!=null&&textBox2.Text.Length>0)
+			{
+				codeRecord.add(textBox2.Text);
+				textBox2.Text = "";
+			}
+			
+			
+		}
 
-
+		void nextCode()
+		{
+			textBox2.Text = codeRecord.rollBack();
+		}
+		void lastCode()
+		{
+			textBox2.Text = codeRecord.rollOut();
+		}
 		private void button9_Click_1(object sender, EventArgs e)
 		{
-			managerM.Run(textBox2.Text).getValue();
-			textBox2.Text = "";
+			managerM.Run(textBox2.Text).getValue(1,2,3);
+			codeInput();
 			textBox2.Focus();
 		}
 
@@ -582,7 +717,7 @@ namespace LabDataHelper
 				double maxV = manager[index].Max;
 				for (int t = 0; t < manager[index].Count; t++)
 				{
-					if (manager[index][t] == minV )
+					if (manager[index][t] == minV)
 					{
 						manager.removeValue(index, t);
 						break;
@@ -590,7 +725,7 @@ namespace LabDataHelper
 				}
 				for (int t = 0; t < manager[index].Count; t++)
 				{
-					if (manager[index][t] == maxV )
+					if (manager[index][t] == maxV)
 					{
 						manager.removeValue(index, t);
 						break;
@@ -616,7 +751,7 @@ namespace LabDataHelper
 			DialogResult r = saveFileDialog1.ShowDialog();
 			if (r == DialogResult.OK)
 			{
-				helper.saveExcel(fsNmae, 2, 2, converter, unit, baseSet != null ? comboBox4.SelectedIndex : -1);
+				helper.saveExcel(fsNmae, 2, 2, converter, unit, refConverter);
 
 			}
 
@@ -665,36 +800,21 @@ namespace LabDataHelper
 
 		private void button12_Click(object sender, EventArgs e)
 		{
-			Process.Start("shutdown", "/s /t 0");
-			double[] angle0 = new double[manager.Count];
-			double[] angle1 = new double[manager.Count];
-			string x = "x=[";
-			string y = "y=[";
-			double x0 = 0;
-			double y0;
-			for (int i = 1; i < manager.Count; i++)
-			{
-				angle0[i] = double.Parse(manager[i].describe.ToString()) * 1000 * 8 - double.Parse(manager[i - 1].describe.ToString()) * 1000 * 8;
-
-
-				angle1[i] = Math.Abs(converter(manager[i].Mean - manager[i - 1].Mean) - angle0[i]) / angle0[i];
-
-				if (i == manager.Count - 1)
-				{
-					x += angle0[i] + "]";
-					y += angle1[i] + "]";
-				}
-				else
-				{
-					x += angle0[i] + ",";
-					y += angle1[i] + ",";
-				}
-			}
-
-			richTextBox3.Text = x + "\n" + y;
+			comboBox4.SelectedItem = null;
+			comboBox4.Text = "";
 		}
 
 		private void richTextBox3_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void checkBox2_CheckedChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void numericUpDown4_ValueChanged(object sender, EventArgs e)
 		{
 
 		}
---
+++ PipeHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabDataHelper
+{
+	enum InfoType:byte
+	{
+		Stop,Move,MoveTo,Update
+	}
+	public class PipeHelper
+	{
+		string name;
+		int bufferSizeSend = 9;
+		int bufferSizeReceive = 1024;
+		public event Action<byte[]> beforeSend;
+		public event Action<byte[]> onReceive;
+
+		Action<Action<byte[]>> operator0;
+		bool run = false;
+		public PipeHelper(string name)
+		{
+			this.name = name;
+		}
+		public PipeHelper setBufferSize(int Send,int Receive)
+		{
+			this.bufferSizeSend = Send;
+			this.bufferSizeReceive = Receive;
+			return this;
+		}
+		public void Operate(Action<byte[]> Operator) {
+		
+			if(operator0!=null&&Operator!=null)
+			{
+				operator0(Operator);
+			}
+		}
+		public void stop()
+		{
+			run = false;
+		}
+		public void start()
+		{
+			run = true;
+			Task.Run(() =>
+			{
+				byte[] send = new byte[bufferSizeSend];
+				byte[] receive = new byte[bufferSizeReceive];
+				operator0 = a => { lock (send) { a(send); } };
+				try
+				{
+					using(NamedPipeClientStream ncs=new NamedPipeClientStream(name))
+					{
+						ncs.Connect();
+						while(run)
+						{
+							lock(send)
+							{
+                            beforeSend(send);
+							while (send[0] == 0) ;
+							ncs.Write(send);
+							send[0] = 0;
+							}
+							
+							ncs.Read(receive,0,receive.Length);
+							onReceive(receive);
+						}
+					}
+				}
+				catch (Exception e)
+				{
+
+				}
+
+			});
+		}
+	}
+}
